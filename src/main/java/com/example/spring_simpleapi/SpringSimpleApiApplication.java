e330682d0a83be0bd7d0853168346b1dd34a3565
pshsh910 <pshsh910@gmail.com>
2023-03-30 21:37:55 +0900

    init project

---
+++ src/main/java/com/example/spring_simpleapi/SpringSimpleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_simpleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSimpleApiApplication.class, args);
+    }
+
+}
